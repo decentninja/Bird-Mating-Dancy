93cec862b2f72c91e425fe675a7b7856378b9348
decentninja <very@decent.ninja>
2016-01-30 00:12:31 +0100

    Proximity

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+
+	public float jumpDistance = 5f;
+	public Date date;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			transform.Translate(-jumpDistance, 0, 0);
+			date.playerJumped();
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			transform.Translate(jumpDistance, 0, 0);
+			date.playerJumped();
+		}
+	}
+}
