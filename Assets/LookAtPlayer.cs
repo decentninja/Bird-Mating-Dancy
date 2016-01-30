8dddbd9262b2c750dd9914c4a82905c37749ac9e
Stoil <stoilstoychev@gmail.com>
2016-01-30 06:16:45 +0100

    Eyes now follow player and are also super wobbly

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private float radius;
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        body.AddForce((player.transform.position - transform.position).normalized * 100);
+
+    }
+}
